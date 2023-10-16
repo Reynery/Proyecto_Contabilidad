57734968a03f4388d40be8dda366213318f45242
Josue Morales <moralesjosuereyneri@gmail.com>
2023-10-16 15:36:59 -0600

    Se trabajo en el Forms_Categorias.cs

--- Sistema_De_Costos/Form_Categorias.cs
+++ Sistema_De_Costos/Form_Categorias.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sistema_De_Costos.Entidades;
+using Sistema_De_Costos.Negocio;
 
 namespace Sistema_De_Costos
 {
@@ -16,6 +19,29 @@ namespace Sistema_De_Costos
         {
             InitializeComponent();
         }
+
+        #region "Mis Métodos"
+            private void Formato_ca()
+        {
+            dataGridView_Principal.Columns[0].Width = 100;
+            dataGridView_Principal.Columns[0].HeaderText = "codigo_ca";
+            dataGridView_Principal.Columns[1].Width = 300;
+            dataGridView_Principal.Columns[1].HeaderText = "CATEGORIA";
+        }
+
+            private void Listado_ca(string cTexto)
+        {
+            try
+            {
+                dataGridView_Principal.DataSource = N_Categorias.Listado_ca(cTexto);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+
+        }
+        #endregion
 
         private void label1_Click(object sender, EventArgs e)
         {
